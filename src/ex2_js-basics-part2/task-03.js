d7fd06ecdde947cc5ddbd575e24282a18883fd45
Daniil Kuristin <danil.kruit@yandex.ru>
2019-09-26 22:08:38 +0300

    is-else statements improvements

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -1,18 +1,15 @@
 const checkWhatInArray = function (array) {
-    let evenNumbers = 0; // четные числа
-    let oddNumbers = 0; // нечетные
-    let zeroNumbers = 0; // нули
+    let evenNumbers = 0, // четные числа
+        oddNumbers = 0, // нечетные
+        zeroNumbers = 0; // нули
     array.forEach(item => {
-      if (typeof item !== 'number')
-          {return;}
+      if (typeof item !== 'number') return;
       if (item === 0) {
           ++zeroNumbers;
           return;
       }
-      if (item % 2 === 0)
-          {++evenNumbers;}
-      else
-          {++oddNumbers;}
+      if (item % 2 === 0) ++evenNumbers;
+      else ++oddNumbers;
   });
 
     return [evenNumbers,oddNumbers,zeroNumbers];
