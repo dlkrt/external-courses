b6b8a5ece0702cc557ba3b6f8973447e0fd8cd2d
Daniil Kuristin <danil.kruit@yandex.ru>
2019-10-03 02:17:35 +0300

    removed .trim() in task-03, made another solve

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -1,5 +1,7 @@
 function trimStr(str) {
-  return str.trim();
+  let resultStr = (str[0] === ' ') ? str.substring(1) : str;
+  resultStr = (resultStr[resultStr.length-1] === ' ') ? resultStr.substring(0,resultStr.length-1) : resultStr;
+  return resultStr;
 }
 
 module.exports = trimStr;
