d18b70a17a53d7f342edc619e78eefeb49bb33c1
Daniil Kuristin <danil.kruit@yandex.ru>
2019-10-10 22:36:21 +0300

    function-solution without 'this'

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,23 +1,44 @@
-const Calculator = {
-  current: 0,
-  add(value) {
-    if (!isNaN(value) && typeof value === "number") this.current += value;
-  },
-  subtract(value) {
-    if (!isNaN(value) && typeof value === "number") this.current -= value;
-  },
-  divide(value) {
-    if (!isNaN(value) && typeof value === "number") this.current /= value;
-  },
-  multiply(value) {
-    if (!isNaN(value) && typeof value === "number") this.current *= value;
-  },
-  reset() {
-    this.current = 0;
-  },
-  getResult() {
-    return this.current;
-  }
-};
+const calc = (function () {
+  let current = 0;
 
-module.exports = (new Calculator);
+  function add(value) {
+    if (!isNaN(value) && typeof value === "number") current += value;
+    return add;
+  }
+
+  function subtract(value) {
+    if (!isNaN(value) && typeof value === "number") current -= value;
+    return subtract;
+  }
+
+  function divide(value) {
+    if (!isNaN(value) && typeof value === "number") current /= value;
+    return divide;
+  }
+
+  function multiply(value) {
+    if (!isNaN(value) && typeof value === "number") current *= value;
+    return multiply;
+  }
+
+  function getResult() {
+    return current;
+  }
+
+  function reset() {
+    current = 0;
+    return reset;
+  }
+
+  return {
+    add,
+    subtract,
+    divide,
+    multiply,
+    getResult,
+    reset
+  }
+}());
+
+
+module.exports = calc;
