87ed131f6cebc063d43e2bc94bb508671d3017ba
Daniil Kuristin <danil.kruit@yandex.ru>
2019-10-11 21:06:54 +0300

    removed redundant 'const' in task-01

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -1,6 +1,6 @@
 function mySlice(array, begin = 0, end = array.length) {
-  const startIndex = (begin < 0) ? array.length + begin : begin;
-  const endIndex = (end < 0) ? array.length + end : end;
+  const startIndex = (begin < 0) ? array.length + begin : begin,
+    endIndex = (end < 0) ? array.length + end : end;
   return array.filter((item, index) => (index >= startIndex && index < endIndex)); // .filter можно заменить на функцию из task-04
 }
 
