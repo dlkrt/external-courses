2cbf026f49d8d82927d56484145597f366d7890f
Daniil Kuristin <danil.kruit@yandex.ru>
2019-11-06 02:29:22 +0300

    feat(task-02): create and destroy menu by click on it, by elements in task-02.js

--- src/ex12_js_dom/task-02.js
+++ src/ex12_js_dom/task-02.js
@@ -1,11 +1,39 @@
+const menuItems = [
+  {
+    id: 'myAccount',
+    text: 'My account',
+    url: '#'
+  },
+  {
+    id: 'myTasks',
+    text: 'My tasks',
+    url: '#'
+  },
+  {
+    id: 'logOut',
+    text: 'Log out',
+    url: "#"
+  }
+];
+
 profile.onclick = function () {
-  let elem = document.querySelector('.profile .content'),
-    arrow = document.querySelector('.profile .user-menu');
-  if (elem.style.display === 'block') {
-    elem.style.display = 'none';
+  const arrow = document.querySelector('.profile .user-menu'),
+    menu = document.querySelector('.user-menu__content');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
     arrow.style.transform = 'rotate(0)';
+    while (menu.firstChild) menu.removeChild(menu.firstChild);
   } else {
-    elem.style.display = 'block';
+    menuItems.forEach(item => {
+      const newItem = document.createElement('li'),
+        a = document.createElement('a');
+      a.href = item.url;
+      a.innerText = item.text;
+      a.id = item.id;
+      newItem.appendChild(a);
+      menu.appendChild(newItem);
+    });
+    menu.style.display = 'block';
     arrow.style.transform = 'rotate(180deg)';
   }
 };
