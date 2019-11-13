2cae3b690879064edbd2366bd1fffda4dc057563
Daniil Kuristin <danil.kruit@yandex.ru>
2019-11-14 01:59:46 +0300

    fix(task-01): timout error

---
+++ src/ex16_js-async/task-02.js
@@ -0,0 +1,18 @@
+function debounce(func, delay) {
+  let timerId;
+  return function () {
+    if (timerId) clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func.apply(this);
+      timerId = null;
+    }, delay);
+  }
+}
+
+function updateData() {
+  console.log(arguments);
+  document.getElementById('response').innerText = document.getElementById('searchInput').value;
+}
+
+searchInput.oninput = debounce(updateData,500);
