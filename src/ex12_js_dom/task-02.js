898813d294749636550452488504202f57db0d95
Student <stud@Epam.loc>
2019-11-15 20:45:58 +0400

    fix(task-02): addEventListener and div for menu

--- src/ex12_js_dom/task-02.js
+++ src/ex12_js_dom/task-02.js
@@ -16,25 +16,25 @@ const menuItems = [
   }
 ];
 
-profile.onclick = function () {
-  const arrow = document.querySelector('.profile .user-menu');
-  let menu = document.querySelector('.user-menu__content');
-  if (menu) {
-    arrow.style.transform = 'rotate(0)';
-    menu.remove();
-  } else {
-    menu = document.createElement('ul');
-    menu.className = 'user-menu__content';
-    menuItems.forEach(item => {
-      const newItem = document.createElement('li'),
-        a = document.createElement('a');
-      a.href = item.url;
-      a.innerText = item.text;
-      a.id = item.id;
-      newItem.appendChild(a);
-      menu.appendChild(newItem);
-    });
-    profile.appendChild(menu);
-    arrow.style.transform = 'rotate(180deg)';
-  }
-};
+document.querySelector('.profile').addEventListener('click', function () {
+	const arrow = document.querySelector('.profile .user-menu');
+	const menu = document.querySelector('.user-menu__content');
+	if (menu) {
+		arrow.style.transform = 'rotate(0)';
+		menu.remove();
+		return;
+	}
+	const newMenu = document.createElement('ul');
+	newMenu.className = 'user-menu__content';
+	menuItems.forEach(item => {
+	const newItem = document.createElement('li'),
+		a = document.createElement('a');
+		a.href = item.url;
+		a.innerText = item.text;
+		a.id = item.id;
+		newItem.appendChild(a);
+		newMenu.appendChild(newItem);
+	});
+	document.querySelector('.dropdown-user-menu').appendChild(newMenu);
+	arrow.style.transform = 'rotate(180deg)';
+});
