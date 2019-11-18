a7082d166fd1678d0bfaeab0de3037493ca643c6
Daniil Kuristin <danil.kruit@yandex.ru>
2019-11-19 00:29:30 +0300

    fix(task-02): uses prototype

--- src/ex14_js-context/task-02.js
+++ src/ex14_js-context/task-02.js
@@ -1,51 +1,55 @@
 function Hangman(word) {
-  let mask = word.split('').map((letter) => '_').join(''),
-  wrongLetters = [],
-  mistakes = 6;
-
-  this.guess = (letter) => {
-    if (word.indexOf(letter) !== -1 && mask.indexOf(letter) === -1) {
-      mask = mask.split('').map((maskLetter, i) => {
-        if (word[i] === letter) return letter;
-        return maskLetter;
-      }).join('');
-      console.log(mask + ((mask === word) ? ' | You won!' : ''));
-      if (mask === word) {
-        return this.startAgain(word);
-      }
-    } else {
-      wrongLetters.push(letter);
-      mistakes--;
-      if (mistakes < 0) {
-        console.log(`${word} | You lose!`);
-      } else console.log(`wrong letter, errors left ${mistakes} | ${wrongLetters.join(',')}`);
-    }
-    return this;
-  };
-
-  this.getGuessedString = function () {
-    return mask;
-  };
-
-  this.getErrorsLeft = function () {
-    return mistakes;
-  };
-
-  this.getWrongSymbols = function () {
-    return wrongLetters;
-  };
-
-  this.getStatus = function () {
-    return `${mask} | errors left ${mistakes}`;
-  };
-
-  this.startAgain = function (newWord) {
-      mask = newWord.split('').map((letter) => '_').join('');
-      wrongLetters = [];
-      mistakes = 6;
-      return this;
-  };
+  this.mask = word.split('').map((letter) => '_').join('');
+  this.wrongLetters = [];
+  this.mistakes = 6;
+  this.word = word;
 }
+
+Hangman.prototype.guess = function (letter) {
+  if (this.word.indexOf(letter) !== -1 && !this.mask.includes(letter)) {
+    this.mask = this.mask.split('').map((maskLetter, i) => {
+      if (this.word[i] === letter) return letter;
+      return maskLetter;
+    }).join('');
+    console.log(this.mask + ((this.mask === this.word) ? ' | You won!' : ''));
+    if (this.mask === this.word) {
+      return this.startAgain(this.word);
+    }
+  } else {
+    this.wrongLetters.push(letter);
+    this.mistakes--;
+    if (this.mistakes < 0) {
+      console.log(`${this.word} | You lose!`);
+    } else {
+      console.log(`wrong letter, errors left ${this.mistakes} | ${this.wrongLetters.join(',')}`);
+    }
+  }
+  return this;
+};
+
+Hangman.prototype.getGuessedString = function () {
+  return this.mask;
+};
+
+Hangman.prototype.getErrorsLeft = function () {
+  return this.mistakes;
+};
+
+Hangman.prototype.getWrongSymbols = function () {
+  return this.wrongLetters;
+};
+
+Hangman.prototype.getStatus = function () {
+  return `${this.mask} | errors left ${this.mistakes}`;
+};
+
+Hangman.prototype.startAgain = function (newWord) {
+  this.mask = newWord.split('').map((letter) => '_').join('');
+  this.wrongLetters = [];
+  this.mistakes = 6;
+  return this;
+};
+
 
 const hangman = new Hangman('webpurple');
 module.exports = hangman;
